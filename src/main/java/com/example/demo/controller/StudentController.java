a0a0f86ab3f6af70da796571f05e97a2c6879ce4
bala-amypo <bala@amypo.in>
2025-12-16 10:22:10 +0000

    User commit

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -1,50 +1,61 @@
 package com.example.demo.controller;
+
 import java.util.List;
 import java.util.Optional;
-import org. springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org. springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bina.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframcwork.web.bind.annotation.RestController;
-import com.example.deo.entity.Student;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.Student;
 import com.example.demo.service.StudentService;
-@CrossOrigin(origins = " *= )
-@RestControllar
+
+@CrossOrigin(origins = "*")
+@RestController
 public class StudentController {
-@Autawired
-StudentService studentService,
-@PostMappim("/PostStudent")
-public Student postStd(@RequestBody Student st)(
-return studentService.insertStudent(st);
 
-@6etMapping("/getA1l")
-public List<Student> getAlI()
-return studentService.getAl1Students();
+    @Autowired
+    StudentService studentService;
 
-@GetMapping("/get/{id}")
-public Optional<Student> get(@PatiVariable Long id)(
-return studentService.getDnestudent(id);
+    @PostMapping("/PostStudent")
+    public Student postStd(@RequestBody Student st) {
+        return studentService.insertStudent(st);
+    }
 
-@PutMapping("/update/(id}"]
-public String update(@PathVariable Long id,@lsquestBody Student
-newStudent){
-Gptidnal<Student> student=studentService.getOneStudent(id);
-if(student.isPresent()){
-newStudent.setId(id);
-studentService.insertStudent (newStudent);
-return "Updated Success";
+    @GetMapping("/getA1l")
+    public List<Student> getAll() {
+        return studentService.getAl1Students();
+    }
 
-return "Id not found";
+    @GetMapping("/get/{id}")
+    public Optional<Student> get(@PathVariable Long id) {
+        return studentService.getOneStudent(id);
+    }
 
-@0eleteMapping("/del/(id}")
-public String deleteStument(@PathVari ble Loig id [
-Opticual<Student> student=studentService.getOneStident(id);
-if(student.isPresent()){
-studentService.deleteitunemt(id);
-return "Deleted Success":
+    @PutMapping("/update/{id}")
+    public String update(@PathVariable Long id, @RequestBody Student newStudent) {
+        Optional<Student> student = studentService.getOneStudent(id);
+        if (student.isPresent()) {
+            newStudent.setId(id);
+            studentService.insertStudent(newStudent);
+            return "Updated Success";
+        }
+        return "Id not found";
+    }
 
-return "Id Not Foumd":
+    @DeleteMapping("/del/{id}")
+    public String deleteStudent(@PathVariable Long id) {
+        Optional<Student> student = studentService.getOneStudent(id);
+        if (student.isPresent()) {
+            studentService.deleteStudent(id);
+            return "Deleted Success";
+        }
+        return "Id Not Found";
+    }
+}
